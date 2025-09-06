9cc7a3fa960a43c691f74e7361fa83cd24b4b9e6
Victoria Bliznets <vbliznets.by@gmail.com>
2025-09-07 02:15:52 +0300

    Add:1.Write the code for the functions in the file hw5_solution.py - to run the tests successfully fromtest_hw5 file; 2.Change the test result to successfully test the test_formatting function in the file test_hw5.py

--- homeworks/hw5/hw5_solution.py
+++ homeworks/hw5/hw5_solution.py
@@ -1,38 +1,44 @@
 def add_ing(s: str) -> str:
-    pass
+    s += 'ing'
+    return s
 
 
 def change_symbol(s: str) -> str:
-    pass
+    return s.replace('#', '/')
 
 
 def change_order(s: str) -> str:
-    pass
+    s1 = s.split()
+    s1[0], s1[1] = s1[1], s1[0]
+    s: str = ' '.join(s1)
+    return s
 
 
 def clean_string(s: str) -> str:
-    pass
+    return s.strip()
 
 
 def to_capitalize(s: str) -> str:
-    pass
+    return s.capitalize()
 
 
 def to_list(s: str) -> list:
-    pass
+    return s.split()
 
 
 def formatting(array: list, s1: str, s2: str) -> str:
-    pass
+    name, lastname = array
+    return f"Привет, {name} {lastname}! Добро пожаловать в {s1} {s2}"
 
 
 def to_string(array: list) -> str:
-    pass
+    return ' '.join(array)
 
 
 def insert_to_list(array: list, item: int | str, indx: int) -> list:
-    pass
-
+    array.insert(indx, item)
+    return array
 
 def delete_from_list(array: list, indx: int) -> list:
-    pass
+    del array[indx]
+    return array
